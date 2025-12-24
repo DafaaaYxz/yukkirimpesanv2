310a43bc7f3c8aaca4d0916ec6f968a82b9e8f74
DafaaaYxz <zeuz8686@gmail.com>
2025-12-24 16:55:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,24 +7,27 @@ const Message = require('./models/Message');
 
 const app = express();
 
+// PERBAIKAN DI SINI: Memberitahu Vercel lokasi folder views secara absolut
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Middleware Auth Sederhana
-const checkAuth = (req, res, next) => {
-    if (!req.cookies.user) return res.redirect('/');
-    next();
-};
-
-// ROUTES
-app.get('/', (req, res) => res.render('index'));
+// Route Dasar
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.post('/register', async (req, res) => {
-    const success = await User.create(req.body.username, req.body.password);
-    if (success) res.send('<script>alert("Berhasil! Silahkan Login."); window.location="/"</script>');
-    else res.send('Username sudah dipakai.');
+    try {
+        const success = await User.create(req.body.username, req.body.password);
+        if (success) res.send('<script>alert("Berhasil! Silahkan Login."); window.location="/"</script>');
+        else res.send('Username sudah dipakai.');
+    } catch (err) {
+        res.status(500).send("Error Database: " + err.message);
+    }
 });
 
 app.post('/login', async (req, res) => {
@@ -33,23 +36,23 @@ app.post('/login', async (req, res) => {
         res.cookie('user', req.body.username);
         res.redirect('/dashboard');
     } else {
-        res.send('Login Gagal.');
+        res.send('Login Gagal. Username/Password salah.');
     }
 });
 
-app.get('/dashboard', checkAuth, async (req, res) => {
+app.get('/dashboard', async (req, res) => {
+    if (!req.cookies.user) return res.redirect('/');
     const msgs = await Message.get(req.cookies.user);
     res.render('dashboard', { user: req.cookies.user, messages: msgs });
 });
 
-// Halaman Public Kirim Pesan
 app.get('/u/:username', (req, res) => {
     res.render('profile', { target: req.params.username });
 });
 
 app.post('/send/:username', async (req, res) => {
     await Message.send(req.params.username, req.body.text);
-    res.send('<center><h1>Pesan Terkirim! 🚀</h1><a href="/u/'+req.params.username+'">Kirim lagi</a></center>');
+    res.send('<center style="background:black; color:white; height:100vh; padding-top:50px; font-family:sans-serif;"><h1>Pesan Terkirim! 🚀</h1><a href="/u/'+req.params.username+'" style="color:cyan;">Kirim lagi</a></center>');
 });
 
 app.get('/logout', (req, res) => {
@@ -57,4 +60,4 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 });
 
-module.exports = app; // Penting untuk Vercel
+module.exports = app;
